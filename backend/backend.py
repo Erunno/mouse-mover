e21db640fb4ae914131e2928a4c6f6f109833750
Matyáš Brabec <matyas.brabec@profinit.eu>
2023-04-12 19:21:01 +0200

    fixed serving of static files

--- backend/backend.py
+++ backend/backend.py
@@ -1,28 +1,38 @@
+import logging
+from flask import send_from_directory, Flask, request, jsonify
 import math
 from time import sleep
 import pyautogui
 pyautogui.FAILSAFE = False
-from flask import send_from_directory, Flask, request, jsonify
-import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 
 width, height = pyautogui.size()
-sensitivity = 800;
+sensitivity = 800
 max_dimension = max(width, height)
 
 app = Flask(
-  __name__,
+    __name__,
+    static_folder='./page'
 )
+
 
 @app.route('/api/sanity-check')
 def check():
     return "app works"
 
+
+@app.route('/')
+def index():
+    return send_from_directory('page', 'index.html')
+
+
+@app.route('/')
 @app.route('/<path:path>')
-def serve_static_files(path):
-    return send_from_directory('static', path)
+def fallback(path=None):
+    return send_from_directory('page', path)
+
 
 @app.route('/api/mouse-vector', methods=['POST'])
 def mouse_vector():
@@ -36,6 +46,7 @@ def mouse_vector():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+
 @app.route('/api/mouse-click', methods=['GET'])
 def mouse_click():
     pyautogui.click()
@@ -44,18 +55,19 @@ def mouse_click():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+
 @app.route('/api/mouse-right-click', methods=['GET'])
 def mouse_right_click():
     pyautogui.rightClick()
-    
+
     response = jsonify({'status': 'Mouse right clicked'})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-    
+
 if __name__ == '__main__':
-  app.run(
-	host='0.0.0.0',
-	debug=False,
-	port=80, 
-)
+    app.run(
+        host='0.0.0.0',
+        debug=False,
+        port=80,
+    )
